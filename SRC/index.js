c84f524b2fd989d08cdf320d50317367d8c1aa55
harilal04 <harilalc66@gmail.com>
2022-12-14 23:38:56 +0530

    completed basic setup of sequelize

--- SRC/index.js
+++ SRC/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const {Port} = require("./config/ServerConfig.js");
+const bodyparser = require("body-parser");
 
 const startSetUpServer = async () =>{
     const app = express();
-    
+    app.use(bodyparser.json);
+    app.use(bodyparser.urlencoded({extended:true}));
     app.listen(Port,() => {
         console.log(`Server started on ${Port}`);
         //console.log(process.env);
