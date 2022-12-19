61f2a77e6706aadfa6e0cb900a0ba2c07077a233
harilal04 <harilalc66@gmail.com>
2022-12-19 14:35:27 +0530

    completed city repository with crud

--- SRC/repository/city-repository.js
+++ SRC/repository/city-repository.js
@@ -1,1 +1,53 @@
-const {city} = require('../models/index.js');
+const {City} = require('../models/index');
+
+class CityRepository{
+    async createCity({ name }){
+        try {
+            const city = await City.create({ name });
+            return city;
+        }
+        catch(error) {
+            console.log("Something went wrong in repository");
+            throw { error };
+        }
+    }
+
+    async deleteCity({ cityId }){
+        try {
+            await City.destroy({where: {id: cityId}})
+        }
+        catch(error) {
+            console.log("Something went wrong in repository");
+            throw { error };
+        }
+    }
+
+    async updateCity( cityId, data ){
+        try{
+           const city =  await City.update( data, {
+                where: {
+                    Id: cityId 
+                }
+            })
+            return city;
+        }
+        catch( error ){
+            console.log("Something went wrong in repository");
+            throw{ error }
+        }
+    }
+
+    async getCity( cityId ){
+        try{
+            const city = await City.findByPk( cityId );
+            return city;
+        }
+        catch( error ){
+            console.log("Something went wrong in repository");
+            throw{ error }
+        }
+        
+    }
+}
+
+module.exports = CityRepository;
--- SRC/server.js
+++ SRC/server.js
@@ -1,14 +1,24 @@
 const express = require("express");
+
 const {Port} = require("./config/ServerConfig.js");
+
 const bodyparser = require("body-parser");
 
+const CityRepository = require("./repository/city-repository");
+
 const startSetUpServer = async () =>{
+
     const app = express();
+
     app.use(bodyparser.json);
     app.use(bodyparser.urlencoded({extended:true}));
-    app.listen(Port,() => {
+
+    app.listen(Port,async () => {
         console.log(`Server started on ${Port}`);
         //console.log(process.env);
+        const repo = new CityRepository();
+       repo.createCity({name : "Mumbai"});
+       
     })
 }
 
