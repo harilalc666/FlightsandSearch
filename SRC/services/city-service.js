a71028f5c5617e88de158c44bb2593a280224fa3
harilal04 <harilalc66@gmail.com>
2022-12-20 22:08:43 +0530

    Added filter query [Op.Like]

--- SRC/services/city-service.js
+++ SRC/services/city-service.js
@@ -48,9 +48,9 @@ class CityService{
         }
     }
 
-    async getAllCities(){
+    async getAllCities(filter){
         try {
-            const cities = await this.cityrepository.getAllCities()
+            const cities = await this.cityrepository.getAllCities({name : filter.name})
             return cities;
         } catch (error) {
             console.log("Something went wrong in Service layer");
